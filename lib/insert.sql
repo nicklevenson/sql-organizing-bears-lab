1a3e0c856125db500fb2b0dffaa71c6b318f1704
Nicholas Levenson <nicklevenson@gmail.com>
2020-11-16 12:06:39 -0800

    commit tree 9bfb02e00bcc0a9bfaa86891135c684d7beae1da

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 1, 'M', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sargeant Brown', 6, 'M', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 9, 'F', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 17, 'M', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'Black', 'Mean', 'alive');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'Black', 'Mean', 'alive');
